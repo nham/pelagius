8b478198550b08199416bb8ca021a6ecc9c18e2b
nham <hamann.nick@gmail.com>
2013-12-19 15:32:06 -0500

    implement a fixed size stack with a static array

--- 1_3/stack.d
+++ 1_3/stack.d
@@ -1,4 +1,5 @@
 class Stack(Item) {
+    Item[] stack;
     void push(Item item) {
 
     }
@@ -13,5 +14,28 @@ class Stack(Item) {
 
     int size() {
 
+    }
+}
+
+class FixedStack(Item) {
+    Item[100] stack;
+    int N;
+    void push(Item item) {
+        stack[N] = item;
+        N += 1;
+    }
+
+    Item pop() {
+        N -= 1;
+        return stack[N+1];
+
+    }
+
+    bool isEmpty() {
+        return N == 0;
+    }
+
+    int size() {
+        return N;
     }
 }
